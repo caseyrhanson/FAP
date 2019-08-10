a0adcdcdfe6c3b4648824873b98ca959d67459c6
Aaron Horning <ahorning@GEN-FVFX528SHV2H.local>
2019-08-09 20:11:44 -0700

    initial commit of r function to select patient specific columns in VAP output file

---
+++ vapReduce_cover,vcover,mutect.R
@@ -0,0 +1,51 @@
+vapReduce_cover_vcover_mutect = function(vap, pt){
+  #make a new talbe with sample_mutect, sample_cover sample_freq
+  # mutect is boolean (yes or no), cover is depth (d), freq is maf (percentage).
+  
+  library(dplyr);library(tidyr);library(stringr)
+  
+  #make vap file patient specific
+  #choose only first columns ("chr", "pos", "id", "ref", "alt") and  columns related to specific patient
+  index.pt = which(grepl(pt,colnames(vap)))
+  vap.reduced.ptspecific = vap[,c(1:6,index.pt)]  
+  
+  
+  vap.names = colnames(vap.reduced.ptspecific)
+  #List out the Sample, Tumor and Normal sample names
+  sample.names = vap.names[which(grepl("maf",vap.names))]
+  sample.names = str_replace(sample.names,pattern = "maf",replacement = "")
+  
+  normal.names = sample.names[grep("blood",sample.names)]
+  tumor.names = sample.names[grep("blood",sample.names,invert = T)]
+  
+  #Select specific columns to keep.
+  ##Keep depth and maf in the reduced table
+  vars = c("CADD_phred", "Polyphen2_HVAR_pred","mutect.snv.res.filtered.classified.founds.flanking.bam.out.bad")
+  vap.reduced = vap.reduced.ptspecific%>%select(chr, pos, id, ref, alt, ends_with("maf"),ends_with("d"),-one_of(vars = vars))
+  index.maf.cols = which(grepl("maf",colnames(vap.reduced)))
+  
+  #determine which maf values are not "0", then label them "yes". Label "0" as "unknown"
+  maf = vap.reduced[,index.maf.cols]
+  maf[maf!=0] = "yes"
+  maf[maf==0] = "unknown"
+  
+  ##Create new columns that say _mutect
+  mutect.names = str_c(sample.names,"_mutect")
+  
+  ##Create new dataframe with all the samples having a new column and each value is "yes"
+  df = maf
+  colnames(df) = mutect.names
+  
+
+  #merge the two dataframes together
+  vap.reduced.and.mutect = cbind(vap.reduced,df)
+  
+
+  
+  
+  return(list(phylip.input.maf.file = vap.reduced.and.mutect,
+              pt.samplenames = sample.names,
+              normals = normal.names,
+              tumor = tumor.names))
+  
+}
